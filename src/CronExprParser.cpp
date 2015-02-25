db4d86271c11e75ad650f15c08b33edcc659e231
alex <alex@staticlibs.net>
2015-02-25 19:45:18 +0000

    more c simplifications

--- src/CronExprParser.cpp
+++ src/CronExprParser.cpp
@@ -163,29 +163,29 @@ unsigned int parse_uint32(const char* str) {
 
 } // namespace
 
-int is_blank(char* st) {
-    size_t len = strlen(st);
-    for(size_t i = 0; i < len; i++) {
-        if(!isspace(st[i])) return 0;
-    }
-    return 1;
-}
+//int is_blank(char* st) {
+//    size_t len = strlen(st);
+//    for(size_t i = 0; i < len; i++) {
+//        if(!isspace(st[i])) return 0;
+//    }
+//    return 1;
+//}
 
-char* trim(char* str) {
-    size_t len = strlen(str);
-    char* out = (char*) malloc(len+1);
-    memset(out, 0, len+1);
-    size_t j = 0;
-    for(size_t i = 0; i < len; i++) {
-        if(!isspace(str[i])) {
-            out[j++] = str[i];
-        }
-    }
-    return out;
-}
+//char* trim(char* str) {
+//    size_t len = strlen(str);
+//    char* out = (char*) malloc(len+1);
+//    memset(out, 0, len+1);
+//    size_t j = 0;
+//    for(size_t i = 0; i < len; i++) {
+//        if(!isspace(str[i])) {
+//            out[j++] = str[i];
+//        }
+//    }
+//    return out;
+//}
 
 
-std::vector<std::string> split_str(std::string pstr, char del) {
+char** split_str(std::string pstr, char del, size_t* len_out) {
 //    size_t len = 0;
 //    char** splitted = str_split(&str.front(), del, &len);
 //    auto res = std::vector<std::string>{};
@@ -195,11 +195,58 @@ std::vector<std::string> split_str(std::string pstr, char del) {
 //    }
 //    return res;
   
-    int accum = 0;
-    size_t len = 0;
+    
+//    int accum = 0;
+//    size_t len = 0;
+//    char* str = &pstr.front();
+//    size_t stlen = strlen(str);
+//    for(size_t i = 0; i < stlen; i++) {
+//        if (del == str[i]) {
+//            if (accum > 0) {
+//                len += 1;
+//                accum = 0;
+//            }
+//        } else if (!isspace(str[i])) {
+//            accum += 1;
+//        }
+//    }
+//    if (accum > 0) {
+//        len += 1;
+//    }
+//    char** res = (char**) malloc(len*sizeof(char*));
+//    char* buf = (char*) malloc(stlen + 1);
+//    memset(buf, 0, stlen + 1);
+//    size_t ri = 0;
+//    size_t bi = 0;
+//    for (size_t i = 0; i < stlen; i++) {
+//        if (del == str[i]) {
+//            if (bi > 0) {
+//                res[ri++] = trim(buf);
+//                memset(buf, 0, stlen + 1);
+//                bi = 0;
+//            }
+//        } else if (!isspace(str[i])) {
+//            buf[bi++] = str[i];
+//            bi += 1;
+//        }
+//    }
+//    if (bi > 0) {
+//        res[ri++] = trim(buf);
+//    }
+//    
+//    
+//    auto vec = std::vector<std::string>{};
+//    vec.reserve(len);
+//    for (size_t i = 0; i < len; i++) {
+//        vec.push_back(res[i]);
+//    }
+//    return vec;
+    
     char* str = &pstr.front();
     size_t stlen = strlen(str);
-    for(size_t i = 0; i < stlen; i++) {
+    int accum = 0;
+    size_t len = 0;
+    for (size_t i = 0; i < stlen; i++) {
         if (del == str[i]) {
             if (accum > 0) {
                 len += 1;
@@ -209,57 +256,46 @@ std::vector<std::string> split_str(std::string pstr, char del) {
             accum += 1;
         }
     }
+    // tail
     if (accum > 0) {
+        // copy here
         len += 1;
     }
-    char** res = (char**) malloc(len*sizeof(char*));
+
     char* buf = (char*) malloc(stlen + 1);
     memset(buf, 0, stlen + 1);
-    size_t ri = 0;
+    char** res = (char**) malloc(len * sizeof(char*));
     size_t bi = 0;
+    size_t ri = 0;
     for (size_t i = 0; i < stlen; i++) {
         if (del == str[i]) {
             if (bi > 0) {
-                res[ri++] = trim(buf);
+                // copy here
+//                res.push_back(std::string(buf));
+                res[ri++] = strdup(buf);
                 memset(buf, 0, stlen + 1);
                 bi = 0;
             }
-        } else if (!isspace(str[i])) {
-            buf[bi++] = str[i];
-            bi += 1;
+        } else if (!std::isspace(str[i])) {
+            buf[bi++] = str[i];            
         }
     }
+    // tail
     if (bi > 0) {
-        res[ri++] = trim(buf);
+        // copy here
+//        res.push_back(std::string(buf));
+        res[ri++] = strdup(buf);
     }
+    *len_out = len;
+    return res;
+//    assert(len == res.size());
     
-    
-    auto vec = std::vector<std::string>{};
-    vec.reserve(len);
-    for (size_t i = 0; i < len; i++) {
-        vec.push_back(res[i]);
-    }
-    return vec;
-    
-//    auto res = std::vector<std::string>{};
-//    auto cur = std::string{};
-//    for (auto ch : str) {
-//        if (del == ch) {
-//            if (cur.length() > 0) {
-//                // copy here
-//                res.push_back(cur);
-//                cur.clear();
-//            }
-//        } else if (!std::isspace(ch)) {
-//            cur.push_back(ch);
-//        }
+//    auto vec = std::vector<std::string>{};
+//    vec.reserve(len);
+//    for (size_t i = 0; i < len; i++) {
+//        vec.push_back(res[i]);
 //    }
-//    // tail
-//    if (cur.length() > 0) {
-//        // copy here
-//        res.push_back(cur);
-//    }
-//    return res;
+//    return vec;
 }
 
 std::string replace_ordinals(std::string value, const std::vector<std::string>& arr) {
@@ -281,13 +317,14 @@ std::pair<uint32_t, uint32_t> get_range(std::string field, uint32_t min, uint32_
         res.first = val;
         res.second = val;
     } else {
-        auto parts = split_str(field, '-');
-        if (parts.size() > 2) {
+        size_t len = 0;
+        char** parts = split_str(field, '-', &len);
+        if (len > 2) {
             throw CronParseException("Range has more than two fields: '" +
                     field + "' in expression \"" + "this.expression" + "\"");
         }
-        res.first = parse_uint32(parts[0].c_str());
-        res.second = parse_uint32(parts[1].c_str());
+        res.first = parse_uint32(parts[0]);
+        res.second = parse_uint32(parts[1]);
     }
     if (res.first >= max || res.second >= max) {
         throw std::exception();
@@ -302,27 +339,38 @@ std::pair<uint32_t, uint32_t> get_range(std::string field, uint32_t min, uint32_
     return res;
 }
 
+int has_chars(char* str, char ch) {
+    size_t len = strlen(str);
+    for (size_t i = 0; i < len; i++) {
+        if (str[i] == ch) return 1;
+    }
+    return 0;
+}
+
 char* set_number_hits(std::string value, uint32_t min, uint32_t max) {
     char* bits = (char*) malloc(max);
     memset(bits, 0, max);
-    std::vector<std::string> fields = split_str(value, ',');
-    for (auto& field : fields) {
-        if (std::string::npos == field.find('/')) {
+    size_t len = 0;
+    char** fields = split_str(value, ',', &len);
+    for (size_t i = 0; i < len; i++) {
+        char* field = fields[i];
+        if (!has_chars(field, '/')) {
             // Not an incrementer so it must be a range (possibly empty)
             auto range = get_range(field, min, max);
             for (uint32_t i = range.first; i <= range.second; i++) {
                 bits[i] = 1;
             }
         } else {
-            std::vector<std::string> split = split_str(field, '/');
-            if (split.size() > 2) {
+            size_t len2 = 0;
+            char** split = split_str(field, '/', &len2);
+            if (len2 > 2) {
                 throw CronParseException("Incrementer has more than two fields: '");
             }
             auto range = get_range(split[0], min, max);
-            if (std::string::npos == split[0].find("-")) {
+            if (!has_chars(split[0], '-')) {
                 range.second = max - 1;
             }
-            auto delta = parse_uint32(split[1].c_str());
+            auto delta = parse_uint32(split[1]);
             for (uint32_t i = range.first; i <= range.second; i += delta) {
                 bits[i] = 1;
             }
@@ -366,14 +414,15 @@ char* set_days_of_month(std::string field) {
 
 
 CronExprBitsets parse(std::string expression) {
-    auto fields = split_str(expression, ' ');
-    if (fields.size() != 6) {
+    size_t len = 0;
+    char** fields = split_str(expression, ' ', &len);
+    if (len != 6) {
         throw CronParseException("Cron expression must consist of 6 fields (found %d in \"%s\")");
     }
     auto seconds = set_number_hits(fields[0], 0, 60);
     auto minutes = set_number_hits(fields[1], 0, 60);
     auto hours = set_number_hits(fields[2], 0, 24);
-    to_upper(&fields[5].front());
+    to_upper(fields[5]);
     auto days_of_week = set_days(replace_ordinals(fields[5], DAYS), 8);
     if (days_of_week[7]) {
         // Sunday can be represented as 0 or 7
